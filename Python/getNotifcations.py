53ab35e3764ed242d0965e03b78156a91412a8a0
Jeremy Valance <jvalance@pcm-jvalance-1.local>
2018-03-06 08:41:46 -0500

    Added example get request

---
+++ Python/getNotifcations.py
@@ -0,0 +1,23 @@
+import http.client
+import json
+import authorization
+
+class HubNotifications:
+    def getNotifications(self):
+        try:
+            authorize = authorization.HubAuthorization()
+            bearerToken = authorize.authorizeHub()          
+            hubConn = http.client.HTTPSConnection(authorization.HUB_CONFIG['host'])
+            headers = {
+                'authorization': 'bearer ' + bearerToken,
+                'content-type': 'application/json'
+            }
+            hubConn.request('GET', '/api/projects', headers=headers)
+            res = hubConn.getresponse()
+            data = res.read()
+            print(data)
+        except Exception as e:
+            print(e)
+    
+notifications = HubNotifications()
+notifications.getNotifications()
