44caea3450366b2fb2df98ae9bee2f39fb30e144
Jeremy Valance <jvalance@pcm-jvalance-1.local>
2018-03-06 08:42:57 -0500

    Updated python

--- Python/authorization.py
+++ Python/authorization.py
@@ -5,8 +5,8 @@ import json
 # Reminder to add 'token ' before the auth token from the Hub
 
 HUB_CONFIG = {
-    'host': 'huburl.com',
-    'token': ''
+    'host': 'hub.com',
+    'token': 'token '
 }
 
 # Hub Connect
@@ -26,13 +26,13 @@ class HubAuthorization:
             # Pass bearer token as auth headers in subsequent requests
             bearerToken = auth_obj['bearerToken']
             print('Hub Authorization Succeeded.')
+            return bearerToken
 
         except Exception as e:
             print('Cannot connect to Hub. Invalid token.')
             print(e)
 
-authorize = HubAuthorization()
-authorize.authorizeHub()
 
 
-
+#authorize = HubAuthorization()
+#authorize.authorizeHub()
